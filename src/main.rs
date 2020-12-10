87c3c8cb1ed12aead716f37afcbe2035c54f9970
kevin <krecoding@gmail.com>
2020-12-10 15:57:47 -0500

    module for init hashMap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+mod initialization;
+
 use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let challenge = &args[1];
+    initialization::hello();
+
+    println!("Running rust-cryptopals!");
+    println!("Selected challenge {}", challenge);
 }
